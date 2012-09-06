54146b78f3022ae9e61ca78e28a97940d2221cb0
Jeppe Andreasen <jeppe.andreasen@linqit.dk>
2012-09-06 12:37:48 +0200

    Updated Versions of LinqIt Library

--- UmbracoPublic.WebSite/umbraco/masterpages/Site.Master.designer.cs
+++ UmbracoPublic.WebSite/umbraco/masterpages/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace UmbracoPublic.WebSite.umbraco.masterpages {
     public partial class Site {
         
         /// <summary>
+        /// litTitle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litTitle;
+        
+        /// <summary>
         /// litDynamicMetaTags control.
         /// </summary>
         /// <remarks>
