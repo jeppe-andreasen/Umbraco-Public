465aa62496dc8ac70ec21ceea232c749cb9126f4
Jeppe Andreasen <jeppe.andreasen@linqit.dk>
2012-09-06 12:13:59 +0200

    Created gitignore

---
+++ UmbracoPublic.Logic/Modules/ContactList/ContactListModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UmbracoPublic.Logic.Modules.Contact;
+
+namespace UmbracoPublic.Logic.Modules.ContactList
+{
+    public class ContactListModule : BaseModule
+    {
+        public string Headline
+        {
+            get { return GetValue<string>("headline"); }
+        }
+
+        public IEnumerable<ContactModule> Contacts
+        {
+            get { return GetEntities<ContactModule>("contacts"); }
+        }
+    }
+}
---
+++ UmbracoPublic.Logic/Modules/ContactList/ContactListModuleRendering.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace UmbracoPublic.Logic.Modules.ContactList
+{
+    public class ContactListModuleRendering : BaseModuleRendering<ContactListModule>
+    {
+        protected override void RenderModule(ContactListModule item, LinqIt.Utils.Web.HtmlWriter writer)
+        {
+            writer.RenderBeginTag(HtmlTextWriterTag.Ul, "thumbnails");
+            writer.RenderBeginTag(HtmlTextWriterTag.Li, "span3");
+
+            if (!string.IsNullOrEmpty(item.Headline))
+                writer.RenderFullTag(HtmlTextWriterTag.H3, item.Headline);
+
+            foreach (var contact in item.Contacts)
+                Snippets.RenderContact(writer, contact, null);
+
+            writer.RenderEndTag(); // li.span3
+            writer.RenderEndTag(); // ul.thumbnails
+
+
+
+
+        }
+    }
+}
